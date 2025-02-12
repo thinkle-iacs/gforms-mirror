087b13b959a710264781f149438f2c48eded1742
Tom Hinkle <tmhinkle@gmail.com>
2025-02-11 22:32:29 -0500

    ++new methods

--- src/svelte/gasApi.ts
+++ src/svelte/gasApi.ts
@@ -19,12 +19,48 @@ export const GoogleAppsScript = {
       });
     },
 
+     getFormIdFromUrl(formUrl: string): Promise<string> {
+      return new Promise((resolve, reject) => {
+        google.script.run
+          .withSuccessHandler((result: string) => resolve(result))
+          .withFailureHandler((error: any) => reject(error))
+          .getFormIdFromUrl(formUrl);
+      });
+    },
+
      getCachedFormData(formId: string, formUrl: string): Promise<any> {
       return new Promise((resolve, reject) => {
         google.script.run
           .withSuccessHandler((result: any) => resolve(result))
           .withFailureHandler((error: any) => reject(error))
           .getCachedFormData(formId, formUrl);
+      });
+    },
+
+     getTranslationsSpreadsheetUrl(formId: any, initialLanguages: string[]): Promise<string> {
+      return new Promise((resolve, reject) => {
+        google.script.run
+          .withSuccessHandler((result: string) => resolve(result))
+          .withFailureHandler((error: any) => reject(error))
+          .getTranslationsSpreadsheetUrl(formId, initialLanguages);
+      });
+    },
+
+     setupTranslationsSpreadsheet(formId: any, initialLanguages: string[]): Promise<string> {
+      return new Promise((resolve, reject) => {
+        google.script.run
+          .withSuccessHandler((result: string) => resolve(result))
+          .withFailureHandler((error: any) => reject(error))
+          .setupTranslationsSpreadsheet(formId, initialLanguages);
+      });
+    },
+
+     getTranslations(formId: any): Promise<import("../gas/types").Translations> {
+      return new Promise((resolve, reject) => {
+        google.script.run
+          .withSuccessHandler((result: import("../gas/types").Translations) => resolve(result))
+          .withFailureHandler((error: any) => reject(error))
+          .getTranslations(formId);
       });
     },
 
